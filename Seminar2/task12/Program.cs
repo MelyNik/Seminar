32b50f79a548cd14498d0fbd0d3372aa3cbc1b50
Nikita <number_nik@mail.ru>
2022-08-10 12:15:47 +0300

    Решил задачи Семинара 2

--- Seminar2/task12/Program.cs
+++ Seminar2/task12/Program.cs
@@ -4,3 +4,15 @@
 Пример:
 14 -> нет 46 -> нет 161 -> да
 */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 & number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно 7 или 23");
+}
--- Seminar2/task13/Program.cs
+++ Seminar2/task13/Program.cs
@@ -7,3 +7,18 @@
 25, 5 -> да 
 8,9 -> нет
 */
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 * number1 == number2 || number2 * number2 == number1)
+{
+    Console.WriteLine($"{number1}, {number2} -> да");
+}
+else
+{
+    Console.WriteLine($"{number1}, {number2} -> нет");
+}
