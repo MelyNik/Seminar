4c2a988de25f877c2687a78102651c192bea9a48
Nikita <number_nik@mail.ru>
2022-08-16 17:48:52 +0300

    Seminar 4

---
+++ Seminar4/task20/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Пример:
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+string NumberOfDigits(string text)
+{
+    string Length = text.Length;
+    Length = Convert.ToInt32;
+    return Length;
+}
+
+Console.WriteLine("Введите число: ");
+string text = Console.ReadLine();
+
+int Length = NumberOfDigits(text);
+Console.WriteLine(Length);
---
+++ Seminar4/task22/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+Пример:
+[1,0,1,1,0,1,0,0]
+*/
+
+int Length = 8;
+int[] arr =new int [Length];
+
+void RandomArrayOrder(int[] array)
+{
+    for(int i = 0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    int Langth = array.Length;
+    while(i <Langth)
+    {
+        Console.Write($"{array[i]} ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+RandomArrayOrder(arr);
+PrintArray(arr);
